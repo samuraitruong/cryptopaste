1e14e57034f6e32ef1a3c53d87ebe82ac8ab3e84
samuraitruong <samuraitruong@hotmail.com>
2018-03-31 22:54:27 +1100

    add form

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Home from './containers/home'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app">
+        <header className="app-header">
+          <img src={logo} className="app-header-logo app-logo-spin" alt="logo" />
+          <h1 className="app-header-title">Text Crypto Paste</h1>
         </header>
-        <div className="App-intro">
+        <div className="container app-main">
          <Home />
         </div>
       </div>
--- src/containers/home.js
+++ src/containers/home.js
@@ -1,14 +1,59 @@
 import React, {Component} from 'react'
+import {Form, FormGroup, ControlLabel, FormControl, HelpBlock, InputGroup, Button} from 'react-bootstrap'
 import HomeActions from '../redux/home-redux'
 import { connect } from 'react-redux'
 
 class HomeScreen extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            text:null,
+        }
+     }
+     handleChange(event) {
+
+     }
+     getValidationState() {
+
      }
      render() {
          return (
-             <div>  Crypto paste </div>
+             <div className="row mt-5">
+                <div className="col-md-12">
+                    <Form>
+                        <FormGroup
+                        controlId="formBasicText"
+                        validationState={this.getValidationState()}
+                        >
+                        <ControlLabel className="float-left">Enter text you want protect</ControlLabel>
+                        <FormControl
+                            componentClass="textarea" 
+                            value={this.state.text}
+                            placeholder="Enter text"
+                            onChange={this.handleChange}
+                            rows="7"
+                        />
+                        </FormGroup>
+
+                        <FormGroup>
+                        <ControlLabel  className="float-left">Enter password</ControlLabel>
+                            
+                            <InputGroup>
+                            <FormControl type="text" />
+                            <InputGroup.Addon>
+                                <Button bsStyle="primary">Generate Password</Button>
+                            </InputGroup.Addon>
+                            </InputGroup>
+                        </FormGroup>
+
+                        <FormGroup className="text-left">
+                            <Button bsStyle="success">PIN IT</Button>
+                            <Button bsStyle="danger">DELETE</Button>
+                        </FormGroup>
+                    </Form>
+                </div>
+          </div>
+          
          )
      }
 }
