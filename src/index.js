cc4259c8d23e9ec6c35cf669d2550c11976ebced
Truong Nguyen <Truong.Nguyen@fusion5.com.au>
2018-04-13 12:01:30 +1000

    added auto deploy to ci-gh-pages branch

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import HomeActions from './redux/home-redux'
-
-import './index.scss';
-import App from './App';
+import './index.scss'
+import App from './App'
 import store from './redux/store'
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker'
+
 store.dispatch(HomeActions.startSync())
 store.dispatch(HomeActions.detectIp())
 
